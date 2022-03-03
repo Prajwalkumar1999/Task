b356e921cb4e52fdee63fdcb5756d57ff56583b2
Prajwalkumar1999 <prajwalvspp@gmail.com>
2022-03-03 22:08:00 +0530

    first commit

---
+++ Script.js
@@ -0,0 +1,31 @@
+let numbersArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
+  cubeWrapper = document.getElementById('cube-container'), appendCubeElement;
+
+numbersArray.forEach((item, index) => {
+  cubeWrapper.innerHTML += '<li data-item=' + item + '>' + item + '</li>'
+  console.log(cubeWrapper);
+})
+shuffleArray = () => {
+  let index = 0
+  numbersArray = numbersArray.sort(() => { return .5 - Math.random() });
+  cubeWrapper.innerHTML = '';
+  while (index < numbersArray.length) {
+    let cubeItem = document.createElement("li");
+    cubeItem.innerHTML = numbersArray[index]
+    cubeItem.setAttribute('data-item', numbersArray[index])
+    cubeWrapper.appendChild(cubeItem)
+    ++index;
+  }
+}
+sortArray = () => {
+  let index = 0
+  numbersArray = numbersArray.sort((a, b) => { return a - b }); //sorts the array into ascending order.
+  cubeWrapper.innerHTML = ''
+  while (index < numbersArray.length) {
+    let cubeItem = document.createElement("li");
+    cubeItem.innerHTML = numbersArray[index]
+    cubeItem.setAttribute('data-item', numbersArray[index])
+    cubeWrapper.appendChild(cubeItem)
+    ++index;
+  }
+}
